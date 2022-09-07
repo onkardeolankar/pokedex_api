9b6fc2b65d8b1fb32fb0c78d8d5c247b8eef2f06
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-09-07 14:41:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Main from './WorkItems/Main';
+import './WorkItems/style.css'
+
+function App() {
+	return (
+		<>
+		  <Main/>
+		</>
+	  );
+}
+
+export default App;
